9ae0f7cd9bec50c5bec8a627dbc4947c54a3ee66
jpacheco008 <69394703+jpacheco008@users.noreply.github.com>
2021-01-05 16:11:48 -0500

    backend fixes

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,72 +1,67 @@
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken")
-const User = require("../models/userModels")
-const db = require("../db/connection")
+const jwt = require("jsonwebtoken");
+const User = require("../models/userModels");
+const db = require("../db/connection");
 
-db.on("error", console.error.bind(console, "MongoDB connection error:"))
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 const SALT_ROUNDS = 11;
-const TOKEN_KEY = "areallylongkey"
+const TOKEN_KEY = "areallylongkey";
 
 const signUp = async (req, res) => {
   try {
-    const {
-      username, email, password,
-    } = req.body
-    const password_digest = await bcrypt.hash(password, SALT_ROUNDS)
+    const { username, email, password } = req.body;
+    const password_digest = await bcrypt.hash(password, SALT_ROUNDS);
     const user = new User({
-      username, email, password_digest
-    })
-    await user.save()
-    const payload = { username: user.username, email: user.email }
-    const token = jwt.sign(payload, TOKEN_KEY)
-    res.status(201).json({ token })
+      username,
+      email,
+      password_digest,
+    });
+    await user.save();
+    const payload = { username: user.username, email: user.email };
+    const token = jwt.sign(payload, TOKEN_KEY);
+    res.status(201).json({ token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
   }
-  catch (error) {
-    res.status(400).json({error: error.message})
-  }
-}
-
+};
 
 const signIn = async (req, res) => {
   try {
-    const {username, password} = req.body
-    const user = await User.findOne({ username: username })
-    if (await bcrypt.compare(password, user.password_digest)){
+    const { username, password } = req.body;
+    const user = await User.findOne({ username: username });
+    if (await bcrypt.compare(password, user.password_digest)) {
       const payload = {
         username: user.username,
-        email: user.email
-      }
-      const token = jwt.sign(payload, TOKEN_KEY)
-      res.status(201).json({ token })
+        email: user.email,
+      };
+      const token = jwt.sign(payload, TOKEN_KEY);
+      res.status(201).json({ token });
     } else {
-      res.status(401).send("Invalid credentials")
+      res.status(401).send("Invalid credentials");
     }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
-  catch(error){
-    res.status(500).json({ error: error.message })
-
-  }
-}
+};
 
 const verify = async (req, res) => {
   try {
-    const token = req.headers.authorization.split(" ")[1]
-    const payload = jwt.verify(token, TOKEN_KEY)
+    const token = req.headers.authorization.split(" ")[1];
+    const payload = jwt.verify(token, TOKEN_KEY);
     if (payload) {
-      res.json(payload)
+      res.json(payload);
     }
+  } catch (e) {
+    res.status(401).send("Not authorized");
   }
-  catch(e) {
-    res.status(401).send("Not authorized")
-  }
-}
+};
 
-const changePassword = async (req, res) => { }
+const changePassword = async (req, res) => {};
 
 module.exports = {
-    signUp,
-    signIn,
-    verify,
-    changePassword
-}
+  signUp,
+  signIn,
+  verify,
+  changePassword,
+};

550c3c7ebdb76488c11faa286a568d46b0d41346
Gary Grant <garygrant@garys-air.myfiosgateway.com>
2021-01-07 11:30:40 -0500

    layout/nav/footer created

---
+++ client/src/components/shared/Layout/Layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Nav from '../Nav/Nav'
+import Footer from '../Footer/Footer'
+
+const Layout = () => {
+    return (
+        <>
+            <Nav />
+            <Footer />
+        </>
+    );
+};
+
+export default Layout;
---
+++ client/src/components/shared/Nav/Nav.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Nav.css'
+import { Link } from 'react-router-dom';
+
+
+const Nav = () => {
+    return (
+        <>
+            <div className="nav-container">
+                <h1 className="site-title">Cool Runnings</h1>
+                <nav>
+                    <ul>
+                        <Link to="/products"><li><button className="nav-button">test</button></li></Link>
+                        <Link to=""><li><button className="nav-button">Add Item</button></li></Link>
+                        <Link to="/sign-up"><li><button className="nav-button">Sign Up</button></li></Link>
+                        <Link to="/sign-in"><li><button className="nav-button">Sign In</button></li></Link>
+                    </ul>
+                </nav>
+            </div>
+        </>
+    );
+};
+
+export default Nav;
